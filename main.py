c7806a0c8fce0a63453fd8f9425f8ae57c5a1b09
parastoo naderi <roniaania@gmail.com>
2024-11-12 23:53:27 +0330

    The buttons were placed in the form and the functions related to the command were specified

--- main.py
+++ main.py
@@ -10,4 +10,33 @@ app.title("music player")
 app.geometry("400x500")
 
 
+#DEF
+
+def play_music():
+    pass
+
+def stop_music():
+    pass
+
+def next_song():
+    pass
+
+def previous_song():
+    pass
+
+
+#btn
+
+play_button = ctk.CTkButton(app, text="play",fg_color="#603d6e",hover_color="#846262",  command=play_music)
+play_button.pack(pady=5)
+
+stop_button = ctk.CTkButton(app, text="stop",fg_color="#603d6e",hover_color="#846262",  command=stop_music)
+stop_button.pack(pady=5)
+
+next_button = ctk.CTkButton(app, text="next",fg_color="#603d6e",hover_color="#846262",  command=next_song)
+next_button.pack(pady=5)
+
+prev_button = ctk.CTkButton(app, text="previous",fg_color="#603d6e",hover_color="#846262",  command=previous_song)
+prev_button.pack(pady=5)
+
 app.mainloop() 
